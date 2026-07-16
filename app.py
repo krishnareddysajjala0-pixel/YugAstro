252b5c70e43049804a2c44209c02fa643f7908cb
Krishna Reddy sajjala <krishnareddysajjala0@gmail.com>
2026-07-16 20:40:04 +0530

    fix: change sidereal mode from Fagan-Bradley to Lahiri in get_dasha_info to resolve negative dasa values and missing antardasa

--- app.py
+++ app.py
@@ -1665,7 +1665,7 @@ def transit_chart():
     )
 
 def get_dasha_info(birth_info):
-    swe.set_sid_mode(swe.SIDM_FAGAN_BRADLEY)
+    swe.set_sid_mode(swe.SIDM_LAHIRI)
     dob = birth_info.get('dob', '')
     tob = birth_info.get('tob', '')
     name = birth_info.get('name', '')
